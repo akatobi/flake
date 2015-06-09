1c920ecaf09b43f074db65dd93fcdcf257cf6e46
akatobi <akatobi31@outlook.com>
2015-06-09 22:51:17 +0300

    tinymsy

--- addNews.php
+++ addNews.php
@@ -1,19 +1,32 @@
 <html>
     <head>
-    
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <!-- Place inside the <head> of your HTML -->
+<script type="text/javascript" src="tinymce/tinymce.min.js"></script>
+<script type="text/javascript">
+tinymce.init({
+    selector: "#textNews"
+ });
+</script>
+        <script src="jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+        
+        </script>
     </head>
     <body>
 <?php
 include 'bd.php';
-
-
-
 ?>
         
-        
-        <form >
-        
+        <div class="addNews">
+        <form>
+            <p>имя</p>
+            <textarea></textarea>
+            <p>текст</p>
+            <textarea id="textNews" class="newsText"></textarea>
+            <p>файлы</p>
         </form>
-        
+        </div>
     </body>
 </html>
