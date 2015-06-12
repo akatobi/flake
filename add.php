7adc38b6833a0a86c0122b68e1943a1b289cf7dc
akatobi <akatobi31@outlook.com>
2015-06-12 20:47:31 +0300

    update

--- add.php
+++ add.php
@@ -1,17 +1,17 @@
 <?php
 include 'bd.php';
 
-$name = $_POST['newsName'];
-$text = $_POST['textNews'];
+$name = $_POST['login'];
+$pass = $_POST['pass'];
 $user = 'akatobi';
-if ((empty($name)) or (empty($text))) {
+if ((empty($name)) or (empty($pass))) {
 
     echo 'заполните все поля';
 
 }
 else {
     $data = date("Y-m-d G:i");
-    mysql_query("INSERT INTO `news`(`nameNews`, `textNews`, `userAddNews`, `data`) VALUES ('$name','$text','$user','$data')");
+    mysql_query("INSERT INTO `news`(`nameNews`, `textNews`, `userAddNews`, `data`) VALUES ('$name','$pass','$user','$data')");
     echo 'новость успешно добавлена';
     
 }
--- addNews.php
+++ addNews.php
@@ -2,7 +2,11 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="style.css">
-        <script src="jquery.min.js" type="text/javascript"></script>
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="css/input.css">
+        <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
         <script>
         function send() {
             var s=$('#news').serialize();
@@ -27,14 +31,34 @@ include 'bd.php';
 include 'menu.php';
 ?>
       <form id = 'news'>
-          <p>название</p>
-            <input type="text" name="newsName">
-          <p>текст</p>
-            <input type="text" name="textNews">
-            <br/>
-            <br/>
-          <input type="button" value="отправить" onclick="send()">
+          <span class="input input--haruki">
+					<input class="input__field input__field--haruki" type="text" id="input-1" name="login">
+					<label class="input__label input__label--haruki" for="input-1">
+						<span class="input__label-content input__label-content--haruki">First Name</span>
+					</label>
+				</span>
+          <br/>
+          <br/>
+				<span class="input input--haruki">
+					<input class="input__field input__field--haruki" type="password" id="input-2" name="pass">
+					<label class="input__label input__label--haruki" for="input-2">
+						<span class="input__label-content input__label-content--haruki">password</span>
+					</label>
+				</span>
+          <br/>
+          <br/>
+				<span class="input input--haruki">
+					<input class="input__field input__field--haruki" type="text" id="input-3" name="email">
+					<label class="input__label input__label--haruki" for="input-3">
+						<span class="input__label-content input__label-content--haruki">Email</span>
+					</label>
+				</span>
+          <br/>
+          <br/>
+          <input type="button" onclick="send()" value="зарегестрироваться">
         </form>  
-      
+      <script src="js/classie.js"></script>
+		<script src="js/main.js"></script>
+        <script type="text/javascript" src="js/animate.js"></script>
     </body>
 </html>
