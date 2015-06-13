596b6142c571e32dd963b8962c4abd71a14fef95
akatobi <akatobi31@outlook.com>
2015-06-13 11:22:35 +0300

    update

--- perenos.php
+++ perenos.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/style.css">
         <script src="jquery-2.1.4.min.js" type="text/javascript"></script>
         <script type="text/javascript">
        		function click(){
